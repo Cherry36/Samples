fbfec92c6a80aab952a97a09e876b9567f15203a
Cherry36 <charant779@gmail.com>
2024-10-19 11:59:48 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,31 @@
+package com.Sample.MavenApplication2;
+
+import java.util.*;
+
+class App
+{
+     
+    static String generateCaptcha(int n)
+    {
+
+        Random rand = new Random(62); 
+         
+        String chrs = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+      
+        String captcha = "";
+        while (n-->0){
+            int index = (int)(Math.random()*62);
+            captcha+=chrs.charAt(index);
+        }
+           
+        return captcha;
+    }
+    
+    public static void main(String[] args)
+    {
+         
+        String captcha = generateCaptcha(9);
+        System.out.println("Random captcha generator  :");
+        System.out.println(captcha);
+    }
+}
